d441a35f4bf6c4516704c623f59312a99627b673
Saad Mahmud <shuvro.mahmud79@gmail.com>
2025-08-05 11:31:42 -0400

    Add mlflow tracking starter code

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
+import sys
+
+import mlflow
 from sai_rl import SAIClient
 from stable_baselines3 import PPO
+from stable_baselines3.common.logger import HumanOutputFormat, KVWriter, Logger
 
-## Initialize the SAI client
-sai = SAIClient(comp_id="franka-ml-hiring")
+from mlflow_tools import MLflowOutputFormat
 
-## Make the environment
-env = sai.make_env()
+experiment_name = "SB3 Default PPO Example"
+mlflow.set_experiment(experiment_name)
 
-## Define the model
-model = PPO("MlpPolicy", env)
-model.learn(total_timesteps=100)
+loggers = Logger(
+    folder=None,
+    output_formats=[HumanOutputFormat(sys.stdout), MLflowOutputFormat()],
+)
 
-## Benchmark the model locally
-sai.benchmark(model, use_custom_eval=True)
+
+with mlflow.start_run():
+    ## Initialize the SAI client
+    sai = SAIClient(comp_id="franka-ml-hiring")
+
+    ## Make the environment
+    env = sai.make_env()
+
+    ## Define the model
+    model = PPO("MlpPolicy", env)
+    model.set_logger(loggers)
+    model.learn(total_timesteps=100, log_interval=1)
+
+    ## Benchmark the model locally
+    sai.benchmark(model, use_custom_eval=True)
 
 ## Save and submit the model
 # sai.submit("Default PPO template", model)
