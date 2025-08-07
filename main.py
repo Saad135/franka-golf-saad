c6b567e4b35da495b68fea21e23caa028cff8a31
Saad Mahmud <shuvro.mahmud79@gmail.com>
2025-08-06 20:21:33 -0400

    Add params dict

--- main.py
+++ main.py
@@ -16,9 +16,16 @@ loggers = Logger(
 )
 
 
-with mlflow.start_run():
+with mlflow.start_run(log_system_metrics=True) as run:
     ## Initialize the SAI client
     sai = SAIClient(comp_id="franka-ml-hiring")
+
+    # Create params dict
+    params = {
+        "total_timesteps": 2048 * 4,
+    }
+    # Log parameters to MLflow
+    mlflow.log_params(params)
 
     ## Make the environment
     env = sai.make_env()
@@ -26,7 +33,11 @@ with mlflow.start_run():
     ## Define the model
     model = PPO("MlpPolicy", env)
     model.set_logger(loggers)
-    model.learn(total_timesteps=2048 * 4, log_interval=1, progress_bar=True)
+    model.learn(
+        total_timesteps=params["total_timesteps"], log_interval=1, progress_bar=True
+    )
+
+    # Save the model to MLflow
 
     ## Benchmark the model locally
     # sai.benchmark(model, use_custom_eval=True)
