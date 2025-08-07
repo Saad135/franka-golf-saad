d0dfd1fe5d4f36b84a74f3ab03bf840b38f37b7d
Saad Mahmud <shuvro.mahmud79@gmail.com>
2025-08-07 14:36:54 -0400

    Add functions to save and load models with MLflow integration

--- mlflow_tools.py
+++ mlflow_tools.py
@@ -1,8 +1,31 @@
+import os
 from typing import Any, Dict, Tuple, Union
 
 import mlflow
 import numpy as np
+from stable_baselines3 import DDPG
 from stable_baselines3.common.logger import KVWriter
+
+
+# Fuction to save model parameters and log them to MLflow and then delete the model file
+def save_model(model, model_name):
+    """
+    Saves the model and logs it to MLflow.
+    """
+    model.save(model_name)
+    mlflow.log_artifact(f"{model_name}.zip")
+    # Optionally, you can delete the model file after logging
+    os.remove(f"{model_name}.zip")
+
+
+# Function to download the model from MLflow and load it
+def load_model(run_id, artifact_path="model.zip"):
+    """
+    Constructs the artifact URI from run_id and artifact_path, downloads the model from MLflow, and loads it.
+    """
+    artifact_uri = f"runs:/{run_id}/{artifact_path}"
+    mlflow_model_path = mlflow.artifacts.download_artifacts(artifact_uri)
+    return DDPG.load(mlflow_model_path)
 
 
 # Copied from https://stable-baselines3.readthedocs.io/en/master/guide/integrations.html#mlflow
